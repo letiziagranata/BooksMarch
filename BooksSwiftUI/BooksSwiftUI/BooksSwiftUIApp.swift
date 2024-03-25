f4563e014c148a361626337fa647dfc90b2539ff
letiziagranata <148531975+letiziagranata@users.noreply.github.com>
2024-03-25 11:18:50 +0100

    Add files via upload

---
+++ BooksSwiftUI/BooksSwiftUI/BooksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksSwiftUIApp.swift
+//  BooksSwiftUI
+//
+//  Created by Letizia Granata on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BooksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
